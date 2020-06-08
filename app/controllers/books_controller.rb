d72a2713fd2de8011849ecb3f36710e857b36dd5
karin <mina0208.oxo@gmail.com>
2020-06-09 02:31:52 +0900

    [Finish]4-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+  def top
+    
+  end
+
+  def create
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] ="your post was successfully"
+      redirect_to book_path(@book.id)
+    else
+      render "index"
+    end
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] ="your post was successfully"
+      redirect_to book_path(@book.id)
+    else
+      render "edit"
+    end  
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit( :title, :body)
+  end
+
+end
